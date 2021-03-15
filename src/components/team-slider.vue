f5cd9e261a93ef41c3a1236b3b8eb1d1a6450f3a
Jessica <jessicajanekotze@gmail.com>
2021-03-15 17:14:58 +0200

    fixed a typo

--- src/components/team-slider.vue
+++ src/components/team-slider.vue
@@ -10,7 +10,7 @@
                         <div class="col-12 d-flex inner">
                             <div class="left align-self-center text-center text-md-left">
                                 <h1 data-aos="zoom-out-up" data-aos-delay="400" class="title effect-static-text">Say hello</h1>
-                                <p data-aos="zoom-out-up" data-aos-delay="800" class="description">Welcome to The Bit Foundry. We are a creative duo that love creating and building software! We love all things code, engineering and design so we started a little company instead of making babies right away. Say hello, we'd love to see how we can work together.</p>
+                                <p data-aos="zoom-out-up" data-aos-delay="800" class="description">Welcome to The Bit Foundry. We are a creative duo that love creating and building software! We love all things code, engineering and design so we started a company instead of making babies right away. Say hello, we'd love to see how we can work together.</p>
                             </div>
                         </div>
                     </div>
